359384d603528a27f9f4198b1fd5b84b560d3d19
thethien <phandinhthethien@gmail.com>
2019-05-02 13:57:51 +0700

    aa

---
+++ WebApplication5/WebApplication5.Tests/Controllers/VLTeaTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Controllers;
+using WebApplication5.Models;
+
+namespace WebApplication5.Tests.Controllers
+{
+    [TestClass]
+    public class VLTeaTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new AhihiEntities();
+            var controller = new VLTeaController();
+            var result = controller.Index();
+
+            var view = result as ViewResult;
+            Assert.IsNotNull(view);
+            var model = view.Model as List<BubleTea>;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(db.BubleTeas.Count(), model.Count);
+        }
+    }
+}
---
+++ WebApplication5/WebApplication5/Controllers/VLTeaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Controllers;
+using WebApplication5.Models;
+
+
+namespace WebApplication5.Controllers
+{
+    public class VLTeaController : Controller
+    {
+        AhihiEntities db = new AhihiEntities();
+        //
+        // GET: /VLTea/
+        public ActionResult Index()
+        {
+            var model = db.BubleTeas;
+            return View(model.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+	}
+}
