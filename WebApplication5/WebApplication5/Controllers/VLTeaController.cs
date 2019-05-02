90ffceb37bab4a5a3db033e502312d0a70b94852
thethien <phandinhthethien@gmail.com>
2019-05-02 14:28:01 +0700

    aa

--- WebApplication5/WebApplication5/Controllers/VLTeaController.cs
+++ WebApplication5/WebApplication5/Controllers/VLTeaController.cs
@@ -19,6 +19,23 @@ namespace WebApplication5.Controllers
             var model = db.BubleTeas;
             return View(model.ToList());
         }
+        public ActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(BubleTea model) {
+            ValidateBubleTea(model);
+            if (ModelState.IsValid) 
+            {
+                db.BubleTeas.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            return View(model);
+        }
+
+        public void ValidateBubleTea(BubleTea model) { 
+        }
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
