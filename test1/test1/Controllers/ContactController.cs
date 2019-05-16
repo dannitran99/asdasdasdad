2037c5d02852f187b57f0741d88fe7be891d86e2
dannitran99 <dannitran99@gmail.com>
2019-05-16 11:31:40 +0700

    têstDTB

---
+++ test1/test1/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test1.Models;
+
+namespace test1.Controllers
+{   
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        TestEntities db = new TestEntities();
+        public ActionResult Index()
+        {           
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(detail model)
+        {
+            ValidateBubleTea(model);
+            if (ModelState.IsValid)
+            {
+                db.details.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public void ValidateBubleTea(detail model)
+        {
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }   
+}   
